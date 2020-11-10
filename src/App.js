58249a8e6206ea00f28a1be75cbf672371fc308c
Tien Huynh <tienhuynh@advn.vn>
2020-11-10 17:36:28 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React, { Component } from "react";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ImportPage from "./pages/ImportPage";
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ImportPage from './pages/ImportPage';
 
 class App extends Component {
-
-  render() {
-    return (
-      <div className="container">
-        <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/import">
-            <ImportPage />
-          </Route>
-        </Switch>
-        </Router>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='container'>
+				<Router>
+					<Switch>
+						<Route exact path='/'>
+							<HomePage />
+						</Route>
+						<Route path='/import'>
+							<ImportPage />
+						</Route>
+					</Switch>
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,33 +1,88 @@
-import React, { Component } from "react";
-import { PageHeader, Row, Col, Statistic } from "antd";
-import "./style.less";
+import React, { Component } from 'react';
+import { PageHeader, Row, Col, Statistic } from 'antd';
+import './style.css';
 
 const routes = [
-  {
-    path: "/",
-    breadcrumbName: "Trang chủ",
-  },
+	{
+		path: '/',
+		breadcrumbName: 'Trang chủ',
+	},
 ];
 
 class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        <PageHeader
-          className="site-page-header"
-          title="Trang chủ"
-          breadcrumb={{ routes }}
-        />
-        <Row gutter={[16, 24]}>
-          <Col className="gutter-row mb-10" xl={6} md={8} sm={12} xs={24}>
-            <div  className="red-5">
-            <Statistic title="Active Users" value={112893}/>
-            </div>
-          </Col>
-          </Row>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<PageHeader
+					className='site-page-header'
+					title='Trang chủ'
+					breadcrumb={{ routes }}
+				/>
+				<Row gutter={[16, 16]}>
+					<Col
+						className='gutter-row mb-10'
+						xl={8}
+						md={8}
+						sm={12}
+						xs={24}>
+						<div className='ant-statistic-wrapper ant-statistic-red'>
+							<Statistic title='Active Users' value={112893} />
+						</div>
+					</Col>
+					<Col
+						className='gutter-row mb-10'
+						xl={8}
+						md={8}
+						sm={12}
+						xs={24}>
+						<div className='ant-statistic-wrapper ant-statistic-green'>
+							<Statistic title='Voided' value={112893} />
+						</div>
+					</Col>
+					<Col
+						className='gutter-row mb-10'
+						xl={8}
+						md={8}
+						sm={12}
+						xs={24}>
+						<div className='ant-statistic-wrapper ant-statistic-purple'>
+							<Statistic title='Customers' value={112893} />
+						</div>
+					</Col>
+					<Col
+						className='gutter-row mb-10'
+						xl={8}
+						md={8}
+						sm={12}
+						xs={24}>
+						<div className='ant-statistic-wrapper ant-statistic-dark'>
+							<Statistic title='Net Sales' value={112893} />
+						</div>
+					</Col>
+					<Col
+						className='gutter-row mb-10'
+						xl={8}
+						md={8}
+						sm={12}
+						xs={24}>
+						<div className='ant-statistic-wrapper ant-statistic-light-blue'>
+							<Statistic title='Net Sales' value={112893} />
+						</div>
+					</Col>
+					<Col
+						className='gutter-row mb-10'
+						xl={8}
+						md={8}
+						sm={12}
+						xs={24}>
+						<div className='ant-statistic-wrapper ant-statistic-blue'>
+							<Statistic title='Discount' value={112893} />
+						</div>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
 }
 
 export default HomePage;
