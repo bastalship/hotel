64c0be5ad100e545bf711890584f39e4e76c68f9
Ngoc Tien <tienhuynh@advn.vn>
2020-11-14 16:57:36 +0700

    updated

--- src/pages/ImportPage/index.js
+++ src/pages/ImportPage/index.js
@@ -1,73 +1,80 @@
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, Col, notification, Row, Typography } from 'antd';
-import React, { Component } from 'react';
-import Upload from './../../components/Upload';
-import withErrorHandler from './../../withErrorHandler';
-import axios from './../../service/config';
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Col, notification, Row, Typography } from "antd";
+import React, { Component } from "react";
+import Upload from "./../../components/Upload";
+import withErrorHandler from "./../../withErrorHandler";
+import axios from "./../../service/config";
 
 const { Title } = Typography;
 
 class ImportPage extends Component {
-	state = {
-		loading: false,
-		disabled: true,
-	};
+  state = {
+    loading: false,
+    disabled: true,
+  };
 
-	handleImport = () => {
-		this.setState({ loading: true });
-		axios
-			.post('import')
-			.then((res) => {
-				const data = res.data;
-				if(res.status === 200){
-					notification.success({
-						message: 'Thông báo',
-						description: 'Import thành công!',
-					});
-				}
-				if (data && data.errors && data.errors.message) {
-					notification['error']({
-						message: 'Thông báo',
-						description: data.errors.message,
-					});
-				}
-				this.setState({ loading: false });
-			})
-	};
+  handleImport = () => {
+    this.setState({ loading: true });
+    axios
+      .post("import")
+      .then((res) => {
+        const data = res.data;
+        if (res.status === 200) {
+          notification.success({
+            message: "Thông báo",
+            description: "Import thành công!",
+          });
+        }
+        if (data && data.errors && data.errors.message) {
+          notification["error"]({
+            message: "Thông báo",
+            description: data.errors.message,
+          });
+        }
+        this.setState({ loading: false });
+      })
+      .catch(() => {
+		this.setState({ loading: false });
+		notification["error"]({
+            message: "Thông báo",
+            description: data.errors.message,
+          });
+      });
+  };
 
-	changeDisabled = (disabled) => {
-		this.setState({ disabled: disabled });
-	}
+  changeDisabled = (disabled) => {
+    this.setState({ disabled: disabled });
+  };
 
-	render() {
-		const { loading, disabled } = this.state;
-		return (
-			<div>
-				<Row gutter={[36, 24]}>
-					<Col xs={24}>
-						<Title level={3}>Import</Title>
-					</Col>
-				</Row>
-				<Row gutter={[16, 24]}>
-				<Col className='gutter-row' span={24}>
-						<Button
-							type='primary'
-							icon={<UploadOutlined />}
-							block
-							onClick={this.handleImport}
-							loading={loading}
-							disabled={disabled}>
-							Import
-						</Button>
-					</Col>
-					<Col className='gutter-row mb-10' span={24}>
-						<Upload changeDisabled={this.changeDisabled}/>
-					</Col>
-					
-				</Row>
-			</div>
-		);
-	}
+  render() {
+    const { loading, disabled } = this.state;
+    return (
+      <div>
+        <Row gutter={[36, 24]}>
+          <Col xs={24}>
+            <Title level={3}>Import</Title>
+          </Col>
+        </Row>
+        <Row gutter={[16, 24]}>
+          <Col className="gutter-row" span={24}>
+            <Button
+              type="primary"
+              icon={<UploadOutlined />}
+              block
+              onClick={this.handleImport}
+              loading={loading}
+              disabled={disabled}
+            >
+              Import
+            </Button>
+          </Col>
+          <Col className="gutter-row mb-10" span={24}>
+            <Upload changeDisabled={this.changeDisabled} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default withErrorHandler(ImportPage, axios);
